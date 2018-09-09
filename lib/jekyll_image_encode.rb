82458ac4b0acbfa22c265d0594ba7360f5e0aadf
lepon01 <katie@discordmail.com>
2018-09-09 01:11:57 +0100

    Grab link from `page.[input]`, have default link

--- lib/jekyll_image_encode.rb
+++ lib/jekyll_image_encode.rb
@@ -1,4 +1,5 @@
 require "mimemagic"
+require 'json/ext'
 
 module ImageEncodeCache
   @@cached_base64_codes = Hash.new
@@ -17,34 +18,41 @@ module Jekyll
     class ImageEncodeTag < Liquid::Tag
       include ImageEncodeCache
 
-      def initialize(tag_name, url, options)
-        @url = url.strip
+      def initialize(tag_name, var, options)
+        split = var.split(';')
+        @var = split[0].strip
+        @default = split[1].strip
         super
       end
 
       def render(context)
-        encode_image
-      end
-
-      def encode_image
         require 'open-uri'
         require 'base64'
 
         encoded_image = ''
-        image_handle = open(@url)
 
-        if self.cached_base64_codes.has_key? @url
-          encoded_image = self.cached_base64_codes[@url]
-        else
-          # p "Caching #{@url} as local base64 string ..."
-          encoded_image = Base64.strict_encode64(image_handle.read)
-          self.cached_base64_codes.merge!(@url => encoded_image)
+        begin
+          # https://stackoverflow.com/questions/6672007/how-do-you-access-nested-elements-of-a-hash-with-a-single-string-key
+          # Splat and dig, is the answer
+          url = context.registers[:page].dig(*@var.split('.')) || File.join(__dir__, '..', @default)
+          image_handle = open(url)
+
+          if self.cached_base64_codes.has_key? url
+            encoded_image = self.cached_base64_codes[url]
+          else
+            # p "Caching #{@url} as local base64 string ..."
+            encoded_image = Base64.strict_encode64(image_handle.read)
+            self.cached_base64_codes.merge!(url => encoded_image)
+          end
+
+          data_type = MimeMagic.by_magic(image_handle)
+          image_handle.close
+
+          "data:#{data_type};base64,#{encoded_image}"
+        rescue OpenURI::HTTPError => e
+          puts e.message
+          "error!"
         end
-
-        data_type = MimeMagic.by_magic(image_handle)
-        image_handle.close
-
-        "data:#{data_type};base64,#{encoded_image}"
       end
     end
   end
